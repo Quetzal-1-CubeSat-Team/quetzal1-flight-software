12f486e416806b6783a68877c476588fb74ceb07
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-07-05 08:27:58 -0400

    spelling: although
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- EPS/src/EPS/src/SoftwareWire/SoftwareWire.cpp
+++ EPS/src/EPS/src/SoftwareWire/SoftwareWire.cpp
@@ -244,7 +244,7 @@ uint8_t SoftwareWire::requestFrom(uint8_t address, uint8_t size, boolean sendSto
 {
   uint8_t n=0;             // number of valid received bytes. Start with 0 bytes.
 
-  // The stransmission status is set, allthough it is not returned.
+  // The stransmission status is set, although it is not returned.
   // Start with the status : no error
   _transmission = SOFTWAREWIRE_NO_ERROR;
 
